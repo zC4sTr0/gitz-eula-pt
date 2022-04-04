f1fe9b6260badfa1f43c4d21890f1e5dbd1d01d1
Gabriel Castro <38432614+zC4sTr0@users.noreply.github.com>
2022-04-04 05:04:52 -0300

    Original GITZ TOS 2011-2022
    First commit

---
+++ tos-pt.php
@@ -0,0 +1,383 @@
+
+define( 'suporte2', 'Termos de Uso' );
+
+define( 'suporte3', 'Por favor disponibilize um pouco de seu tempo para ler atentamente as regras que se seguem abaixo.
+
+Se você concordar com elas, seja bem vindo a nossa comunidade e divirta-se no GITZ.
+
+
+
+Nada neste site, no servidor ou no fórum é afiliado a empresa criadora do Gunbound, Softnyx. Nenhum lucro é redirecionado para a mesma ou para alguma empresa p&uacute;blica.
+
+O servidor GITZ é voltadado para a diversão privada de pessoas que optam por um atendimento mais organizado e eficaz, tendo seu arrecadamento voltado especificamente para a manutenção e estabilidade do mesmo.
+
+Somos uma equipe séria e convicta de que trabalhamos para disponibilizar entretenimento e diversão.
+
+Sendo assim, seguem-se as regras abaixo.
+
+
+
+
+
+#1 Aceitando o Termo de Uso
+
+***************************
+
+Lendo este termo de uso e concordando com o mesmo, você estará livre para usufruir de nossos serviços, ser alertado através de mensagens (e-mail) sobre mudanças no servidor e muito mais.
+
+Lembre-se, você estará participando de uma comunidade privada nomeada GITZ, a qual NãO possui filiação com a criadora do game entitulado Gunbound.
+
+Você estará sujeito às regras do site e do fórum, dentro ou fora do jogo, independentemente de quem você seja.
+
+O que vem a seguir, referen-se mais especificamente ao servidor in-game.
+
+
+
+#2 Seu Serviço
+
+**************
+
+O nosso website e nosso fórum foram criados para manter a comunicação com vocês jogadores e para disponibilizar um contato mais imediato, seguro e eficaz com qualquer ocorrência que possa vir a prejudicar a diversão de todos.
+
+Diponibilizamos também sistema de ticket para atendimento, telefone direto com o GameMaster e contato via email, tornando com certeza o nosso serviço bem mais rápido.
+
+Caso você venha a visualizar algum ato ilegal dentro ou fora do jogo, por favor nos contate o mais rápido poss&iacute;vel, assim solucionaremos o problema com maior rapidez.
+
+Lembre-se, ao ingressar em nossa comunidade você deve respeito à todos os jogadores, bem como suas raças, cores, credos, etnias ou crenças. Qualquer ato de racismo é considerado infração séria.
+
+
+
+#3 Seu Dever
+
+***********************
+
+Para se jogar no GITZ, você deve inicialmente ler e aceitar nossas regras, concordar com o que dizemos e aceitar o que perguntamos.
+
+Ao se registrar, alguns dados seus se tornam explicitos aos coordenadores do jogo, bem como sua equipe.
+
+Esteja sempre olhando esses termos, pois serão sempre atualizados por nós da Equipe GITZ.
+
+
+
+#4 Registro e Senha
+
+*******************
+
+Para se registrar é necessário a invenção de um &quot;login&quot;, o qual você usará no &quot;launcher&quot; do Gunbound para se conectar ao nosso servidor.
+
+Este login será sempre o mesmo, enquanto você podera mudar o seu Game ID, nome fict&iacute;cio usado dentro do servidor.
+
+Precisará também criar uma senha com até 10 d&iacute;gitos e se poss&iacute;vel trocá-la de mês em mês, garantindo assim, a sua segurança.
+
+Você deve inserir dados originais e corretos, pois nas premiações de eventos, seus dados serão avalidos, qualquer erro você poderá perder o que iria ganhar.
+
+
+
+#5 Contas Inativas
+
+******************
+
+De tempos em tempos o servidor GITZ, afim de reduzir possiveis &quot;Lags&quot;, apaga contas inativas.
+
+Contas com mais de 2 anos de inatividade estão sujeitas a serem deletadas, não sendo possível a recuperação.
+
+
+
+<b>#6 Contas paralelas (Fakes)</b>
+
+*****************
+
+O GunBound GITZ permite a criação de contas paralelas, contanto que não sejam usadas para ganho de terceiros ou pessoal: (free kill, cumprir eventos, falsidade ideológica, se passar por outras pessoas, estelionato, divulgação, etc)
+
+Caso você tenha cometido conduta ilegal perante as regras em uma conta fake, todas as contas relacionadas a esta conta serão banidas, incluindo sua conta original.
+
+
+#7 Comércio e Doação de Contas
+
+*****************
+
+O ato de trocar o dono da conta é permitido em nosso servidor, mas é necessário ter o acompanhamento da equipe de coordenadores do jogo.
+
+Algumas regras devem ser respeitadas, como: 
+- Existe um período mínimo de 1 (um) mes entre uma troca de dono e outra. Neste período não é permitido a troca e caso seja feita a conta é penalizada pela regra de Compartilhamento;
+- A troca de e-mail deve ser realizada após o vendedor confirmar que a conta está vendida com o pagamento realizado.
+- O vendedor deve confirmar os dados perante e-mail para o representante da equipe. O e-mail deve ser enviado do mesmo cadastrado na acc a ser vendida.
+
+		<b><a href="/forum-topico-1.gb">CLIQUE AQUI PARA ACESSAR A REGRA COMPLETA</a></b>
+<p>
+#8 Troca de dados cadastrados
+
+*****************
+
+As unicas informações possíveis de serem alteradas após a criação de conta é: NickName (após alcançar 2.000 GPs), E-mail (caso tenha acesso ao email atual ou caso se comprove que o jogador acessa a conta por um período mínimo de 1 ano), País (caso o país real do jogador esteja incorreto), Senha.
+
+Informações como Login não poderá ser alterado.
+
+
+#9 Transferência de Cash
+
+*****************
+
+Transferir cash ou presentear amigos com itens vendidos por cash é possível desde que respeite os seguintes critérios:
+<ol>
+<li>Ter feito uma doação nos últimos 60 dias;</li>
+<li>Comprovar a posse de conta.</li>
+</ol>
+Para solicitar a transferência é necessário <a href="forum-novotopico-ticket.gb">abrir uma solicitação via ticket</a> e enviar um e-mail para <b>suporte@gitzwc.com</b> solicitando a transferência. Mas atenção! O e-mail utilizado para enviar deve ser o mesmo e-mail cadastrado na conta.
+
+O limite de transferência de cash é a soma de cash recebido em todas as doações feitas nos últimos 60 dias.
+
+
+#10 Nosso Direito
+
+*****************
+
+Todos os personagens, itens e/ou qualquer outro conteúdo do jogo pertence a nós. O cliente tem o direito de usar esses itens até o término da conta.
+
+Temos o direito de punir os jogadores de acordo com as regras. O usuário poderá ser banido eternamente, mesmo que tenha feito compra de cash. 
+Neste caso, o jogador poderá solicitar o RESET de sua conta (todos os GPs, bonificações e Gold são removidos). 
+
+Ao realizar o procedimento de reset, o jogador tem acesso novamente a conta sem perder nada que comprou com o seu dinheiro.
+
+O servidor contrata uma proteção anti cheating com a mais alta técnologia para combate do uso de programas ilegais. Esta proteção monitora, a pedido do ADMIN, a tela do jogador em tempo real.
+
+Você jogador precisa estar ciente que a sua tela pode ser monitorada a qualquer momento.*
+
+*Visando a privacidade dos jogadores, apenas a tela do jogo é monitorada; O monitoramento só acontece quando o jogador esta dentro do servidor, utilizando os nossos serviços.
+
+
+
+
+Se você leu e concordou com nosso Termo de Uso, seja bem vindo à nossa comunidade GITZ.' );
+
+			
+
+define( 'suporte4', 'Regras' );
+
+define( 'suporte5', '<p align="center"><strong>Leia com atenção as Regras do  GITZ</strong></p>
+
+							<p align="justify">As punições são divididas em dois tipos: bloqueio permanente e bloqueio temporário.</p>
+
+						<p align="justify">As punições que resultam em bloqueio permanente fazem que a sua conta nunca mais possa mais ser acessada.</p>
+
+						<p align="justify">Punições de bloqueio temporário funcionam com uma punição temporária porém que, ao juntar 3 banimentos temporários pode se transformar em um bloqueio permanente caso seja necessário.<br />
+
+						</p>
+
+						<br><br>
+
+		<p style="padding:5px; background-color:#FF0000; border:1px solid #FFCC00;"><span style="color:white">As punições descritas nesta área servem apenas como base.<br>O administrador do servidor tem o direito de aplicar um banimento de acordo com cada caso!<br>Utilize o bom senso, o Administrador poderá aplicar um banimento que não esteja descrito nestas regras porém que ache necessário.</span></p><br><br>
+
+		<br>
+
+		<p style="padding:5px; background-color:#f8f6cd; border:1px solid black;"><strong><span style="color:black;text-align: center;display: block;font-size: 20px;font-weight: bold;">Regras Gerais</span></strong><br><br>
+
+						<span class="infodesc">1.0 Comércio de contas:</span><br />
+						Tal ato é liberado em nosso servidor, desde que feito mediante ao intermédio
+						da STAFF. Entre em contato via ticket para obter mais informações.<br>
+
+						<br>
+						<span class="infodesc">1.1 Qualquer uso de hack ou modificação dos arquivos do jogo:</span><br />
+
+							1&ordm; infração - Punição eterna.<br><br>
+
+						<span class="infodesc">1.2 Se passar por GMs para pedir dados pessoais:</span><br />
+
+							1&ordm; infração - Punição eterna.<br><br>
+
+						<span class="infodesc">1.3 Racismo - O que é?</span><br />
+						Racismo é o ato de discriminar e/ou ofender alguém devido a sua cor ou etnia.
+						É o conceito deturpado de imaginar que existe superioridade entre as raças
+						humanas, seja ela negra, branca, parda e etc.
+
+							1&ordm; infração - 72hrs banido<br />
+
+							2&ordm; infração - 168hrs banido<br>
+
+							3&ordm; infração - Ban permanente.<br><br>
+
+						<span class="infodesc">1.4 Compartilhamento de conta - O que é? </span><br />
+						Compartilhamento de conta é o ato de uma ou mais pessoas jogarem na mesma conta.<br>
+						<br>
+						<b>Existe limite de GP que eu possa fazer na conta de algum amigo
+						ou ele fazer na minha?</b><br />
+						Não é tolerado GP nenhum.
+						A partir do momento que duas pessoas joguem na minhas conta esta será devidadmente punida
+						de acordo com as regras.
+
+						 1&ordm; infração - Remoção de 15 dias de GP<br>
+						 2&ordm; infração - Remoção de 45 dias de GP<br>
+						 3&ordm; infração - Ban permanente.<br><br>
+
+						<span class="infodesc">1.5 Roubo de itens ou aceitar itens roubados/ilegais:</span><br />
+
+							1&ordm; infração - Punição eterna.<br><br>
+
+						<span class="infodesc">1.6 Uso de Bugs que atrapalham o jogo (comprar Avatares ilegais, utilizar-se de erros no sistema do jogo ou do site).</span><br />
+
+							1&ordm; infração - Avaliação da Straff (passivo a punição eterna).<br><br>
+
+						<span class="infodesc">1.7 Abuser - O que é ?</span><br />
+						É considerado abuser o exagero de uma sequência contínua de jogos entre os mesmos
+						jogadores que trancedem da exceção para o descomunal, proporcionando aqueles que
+						praticam uma vantagem sob os demais jogadores.
+
+							1&ordm; infração - Remoção dos GPs feito durante estas partidas</span><br />
+						<br>
+						<span class="infodesc">1.8 Postar a localização do Óculos do Dookie (Missão Gitz):</span><br />
+
+							1&ordm; infração - 24hrs banido<br />
+
+							2&ordm;+ infração - 48hrs banido.<br><br>
+
+						<span class="infodesc">1.9 Scam - O que é?</span><br />
+
+						Refere-se a scam todo ato de enganar outros jogadores para beneficio próprio. Exemplo mais comum são títulos de salas prometendo algo há alguém: "JOGA 10 GANHA AVATAR/CASH" induzindo um jogador que normalmente não teria intenção alguma de jogar nesta sala.
+
+							1&ordm; infração - Advertência verbal<br />
+
+							2&ordm; infração - 24hrs banido<br><br>
+
+						<span class="infodesc">1.10 Game Id ofensivo e alusão a hack:</span><br />
+
+							1&ordm; infração - ID alterado automaticamente<br />
+
+							2&ordm; infração - 24hrs banido e o ID alterado automaticamente<br /><br>
+
+						
+
+						<span class="infodesc">1.11 Ofensas ao Gunbound e a Staff Gitz em qualquer meio de comunicação disponível</span><br />
+
+							1&ordm; infração - Avaliação do GM (passivo a punição eterna)<br><br>
+
+						<span class="infodesc">1.12 Qualquer tipo de divulgação ou propaganda não relacionada ao gunbound.</span><br />
+
+							1&ordm; infração - 48hrs banido<br />
+
+							2&ordm; infração - 96hrs banido <br />
+
+							3&ordm; infração - Ban permanente.<br><br>
+
+						<span class="infodesc">1.13 Misoginia - o que é? </span><br />
+						Em respeito ao público feminino, o Gitz adota esta medida que visa prezar pela honra de  nossas jogadoras, porém sem descaracterizar outra regra vigente (ofensa/desrespeito).
+
+						Será considerado infração de misoginia ataques exagerados de ódio e rancor que ferem à honra. 
+
+						Por se tratar de uma questão interpretativa e para garantir total imparcialidade, casos como estes serão tratados em conjunto pelo Staff, fazendo-se valer a maioria. Cabe a todos os jogadores aceitarem o que for decidido.<br>
+						<br>
+
+							1&ordm; infração - 72hrs banido<br />
+
+							2&ordm; infração - 168hrs banido <br />
+
+							3&ordm; infração - Ban permanente.<br><br>
+
+						<span class="infodesc">1.14 Ofensas / desrespeito se encaixe em uma das seguintes categorias:</span><br />
+						<b>Exemplos passíveis de punição:
+						- Racismo/Homofobia/Xenofobia/Discriminação/Misoginia</b><br><br>
+							A fins de deter o uso excessivo de denúncias imponderadas e que por vezes levaram jogadores a punições "banais", mas que por sua vez não deixavam de ser enquadrar como desrespeito, afinal, este é um termo muito abrangente e de modo que um simples "trouxa" pode ser considerado uma falta de respeito, nós tomamos a decisão de não mais tomar partido em atritos corriqueiro entre jogadores, com exceção a casos extremos cujo os quais serão julgados por nós.<br><br>
+
+						
+
+							1&ordm; infração - 72hrs banido + mute global no jogo após o término do banimento por +72 horas<br />
+
+							2&ordm; infração - 168hrs banido + mute global no jogo após o término do banimento por +72 horas <br />
+
+							3&ordm; infração - Ban permanente.<br><br>
+
+							<span class="infodesc">§ Caso a ofensa seja de cunho sexual.</span><br />
+							<b>Exemplos passíveis de ofensas:
+						- vadia, puta, prostituta, vagabunda, piranha, etc.</b><br><br>
+
+							1º+ infração - Mute global no jogo (O tempo do mute será definido através de uma avaliação do GM responsável)<br><br />
+
+
+							<span class="infodesc">1.15 Mal uso da BUZINA.</span><br />
+							- É passível de punição xingamentos via buzina.<br><br>
+
+							1ª Infração - Bloqueio de 15 dias da buzina.<br />
+							2ª Infração - Bloqueio de 30 dias da buzina.<br />
+							3ª Infração - Bloqueio de 60 dias da buzina.<br /><br>
+
+							<b>Observação:</b><br>
+							Os comandos são criados com um propósito de promover melhorias, interatividade e diversidade dentro do servidor. Por isso o mal uso destes  ou de qualquer outro que vier a ser criado são passíveis de punições de acordo com o julgamento da staff.<br><br>
+
+						<span class="infodesc">1.16 Criação de boatos maldosos contra STAFF/Servidor.</span><br />
+						É tudo aquilo infundado cujo não existe prova para se alegar.<br>
+						São mentiras contadas e espalhadas por maus jogadores afins de denegrir a imagem do server/administração. Algo que é extremamente prejudicial pois afeta a seriedade da administração perante seu público (jogadores e futuros jogadores) influenciando negativamente para o não crescimento do servidor.<br><br>
+
+							1&ordm; infração - Avaliação do GM (passivo a punição eterna)<br />
+							<br>
+							<b>Observação:</b><br />
+							- "GM, um cara me acusou de bot/ladrão. Isso é criação de boatos?
+							R: A regra para criação de boatos maliciosos é válida apenas quando se refere a STAFF/SERVIDOR.
+							Não é aplicável entre jogadores. Pois entendemos que não tem o mesmo extremo de consequências para
+							punição maxima ser aplicada.<br /> <br>
+
+						<span class="infodesc">1.17 Free Kill</span><br />
+						No Gunbound GitzWC o free kill se resume a toda e qualquer combinação maliciosa de partidas entre um ou mais jogadores onde o objetivo é facilitar para o outro e gerar vantagens desonestas no jogo.<br><br>
+						<b>Exemplo:</b><br>
+						- Prática conhecida como "Ice 3k de dano" mas que também pode ser feita com outros mobiles.<br>
+						Esta prática se da no x1 e consiste em forçar os mobiles com auxílio de itens de cura a fazer o máximo de dano possível (3k) e consequentemente obter um ganho anormal de Gold e GP durante a partida. Normalmente é feita em combinação, por este motivo a consideramos ilegal.<br><br>
+
+						<b>Existe limite de partidas contra o mesmo jogador? </b><br>
+						R: Não, mas contamos com o bom senso de cada jogador. Se identificarmos partidas que consideremos abuser o GP ganho em partida será retirado da conta e os jogadores notificados. Não recomendamos que se tranquem em uma sala com os mesmos jogadores por longos períodos de tempo.<br><br>
+
+						<br><b>Posso jogar contra um ou mais jogadores usando o mesmo IP?</b><br>
+						R: Sim, no entanto deve-se tomar mais cuidado quando se há uma diferença notória de habilidade entre um e outro.<br>
+						Por exemplo: Em uma sequência de x1 você ganhar 9 de 10 partidas, ou em uma sequência maior em que você ganhe 90% dos jogos.<br>
+						Nessa situação aconselhamos que joguem no mesmo time, pois da mesma forma que poderia ser você jogando honestamente com um amigo, também poderia ser apenas você utilizando 2 computadores para fazer kill.<br><br>
+
+							1&ordm; infração - Remoção de 15 dias de GP<br />
+
+							2&ordm; infração - Remoção de 45 dias de GP<br />
+
+							3&ordm; infração - Ban permanente.<br><br>
+
+							<i>Lembrando que Free kill não está ligado diretamente a número de partidas, quantia de GP ou IP.<br>
+							A análise de FK é baseada em um conjunto de informações podendo ter ou não ligação com quantia de GP, número de partidas ou endereço de IP.<br>
+							Se ficar comprovado, uma conta pode ser perfeitamente banida em apenas uma partida de Free kill. Por tanto, em hipótese alguma arrisquem ir contra as regras.</i><br>
+							<br>
+
+							<span class="infodesc">1.18 Estorno de Avatares:</span><br />
+							O estorno de avatar só é realizado após 72 horas da data de compra do mesmo. A regra serve
+							para avatar shop, web shop e avatares de eventos mensais.
+							Não é possivel pegar estorno de MARQUE PRESENÇA e ROLETA.</p>
+
+						
+
+						<p style="padding:5px; background-color:#f8f6cd; border:1px solid black;"><strong><span style="color:black;text-align: center;display: block;font-size: 20px;">Regras do Fórum<br></span></strong><br>
+
+						<span class="infodesc"> 3.1 Pornografia no fórum:</span><br />
+
+							1&ordm; infração - 48hrs banido<br />
+
+							2&ordm; infração - 96hrs banido<br />
+
+							3&ordm; infração- Ban permanente.<br><br>
+
+						<span class="infodesc">3.2 Scam (enganar outros jogadores para beneficio próprio):</span><br />
+
+							1&ordm; infração - 48hrs banido<br />
+
+							2&ordm; infração - 96hrs banido<br />
+
+							3&ordm; infração- Ban permanente.<br /><br>
+
+						
+
+						<span class="infodesc">3.3 Divulgação de programas ilegais (Hacks, cheats ou Bugs):</span><br />
+
+							- Punição eterna.<br><br>
+
+						<span class="infodesc">3.4 Mau comportamento (xingar jogadores, floodar tópicos, etc):</span><br />
+
+							1&ordm; infração - 24 horas<br />
+
+							2&ordm; infração - 48 horas banido<br />
+
+							3&ordm; infração- Ban permanente.</p><br>
+
+						
+
+						' );
